477f485380c2a4a33cb9251cc0cbe1fad9c4a06e
PioRobot <90993966+PioRobot@users.noreply.github.com>
2022-02-24 17:40:35 -0600

    first commit

---
+++ iron.c.js
@@ -0,0 +1,34 @@
+class Iron{
+	constructor(x,y,height,width)
+	{
+	// assign options to the rubber ball
+
+	var options={
+		restitution:0.8,
+		friction:3,
+		density:30   
+	};
+		this.x=x;
+		this.y=y;
+		this.height=height;
+        this.width=width;
+		this.body=Bodies.rectangle(this.x, this.y,this.width,this.height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push();
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("yellow");
+			//draw the ellipse here to display the rubber ball
+				rect(0,0,this.width,this.height);
+
+			pop();
+	}
+
+}
